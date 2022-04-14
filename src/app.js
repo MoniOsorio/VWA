27da9913f5c1a9368653099b18766dcc93425a58
MoniOsorio <98335388+MoniOsorio@users.noreply.github.com>
2022-04-14 17:23:38 +0200

    Update app.js
    attributes!

--- src/app.js
+++ src/app.js
@@ -18,15 +18,24 @@ function displayTemperature(response){
    let windElement= document.querySelector("#wind");
    let feelElement=document.querySelector("#feels");
    let dateElement= document.querySelector("#date");
+   let iconElement= document.querySelector("#icon");
    temperatureElement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description; 
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML= Math.round(response.data.wind.speed);
 feelElement.innerHTML= Math.round(response.data.main.feels_like);
-dateElement.innerHTML= formatDate(response.data.dt * 1000)
+dateElement.innerHTML= formatDate(response.data.dt * 1000);
+iconElement.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
+iconElement.setAttribute(
+  "alt",
+  response.data.weather[0].description
+);
 }
-let city= "oslo";
+let city= "tegucigalpa";
 let apiKey = "a163a00e6099e0b4d7da2e23a921eeff";
 let apiUrl =
   `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
