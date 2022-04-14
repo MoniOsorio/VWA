cd27fb4a364d33265504f1c3a6a0bb407a08285d
MoniOsorio <98335388+MoniOsorio@users.noreply.github.com>
2022-04-14 17:04:27 +0200

    Update app.js
    date details

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+function formatDate(timestamp) {
+   let date= new Date(timestamp);
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+   let day = days[date.getDay()];
+
+   return `${day} ${hours}:${minutes}   `;
+}
+
+
 function displayTemperature(response){
 
    let temperatureElement= document.querySelector("#temperature");
@@ -6,12 +17,14 @@ function displayTemperature(response){
    let humidityElement=document.querySelector("#humidity");
    let windElement= document.querySelector("#wind");
    let feelElement=document.querySelector("#feels");
+   let dateElement= document.querySelector("#date");
    temperatureElement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description; 
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML= Math.round(response.data.wind.speed);
 feelElement.innerHTML= Math.round(response.data.main.feels_like);
+dateElement.innerHTML= formatDate(response.data.dt * 1000)
 }
 let city= "oslo";
 let apiKey = "a163a00e6099e0b4d7da2e23a921eeff";
